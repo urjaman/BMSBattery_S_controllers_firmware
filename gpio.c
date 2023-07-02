95f134a3fdbb3e4e69ed9905ec41687fe1c64df3
Urja Rannikko <urjaman@gmail.com>
2023-07-02 12:40:24 +0000

    set input pullup on unused pins
    Stuff like this is why i'm .... not feeling super confident
    about the microcontroller knowledge of previous devs.

--- gpio.c
+++ gpio.c
@@ -12,7 +12,27 @@
 
 void gpio_init (void)
 {
-  ;
+	// General MCU principles: every pin should have a defined state;
+	// if not, the pin might float randomly and toggle spuriously and consume current in the input buffer.
+	// Thus, set every unused pin to input pullup.
+	//               LEARN        unk          unk          unk          unk
+	GPIO_Init(GPIOA, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_5 | GPIO_PIN_6,
+				GPIO_MODE_IN_PU_NO_IT);
+	//               unk (no connect)
+	GPIO_Init(GPIOB, GPIO_PIN_3,
+				GPIO_MODE_IN_PU_NO_IT);
+	//               unk          unk
+	GPIO_Init(GPIOC, GPIO_PIN_4 | GPIO_PIN_6,
+				GPIO_MODE_IN_PU_NO_IT);
+	//               SWIM         unk          unk
+	GPIO_Init(GPIOD, GPIO_PIN_1 | GPIO_PIN_3 | GPIO_PIN_4,
+				GPIO_MODE_IN_PU_NO_IT);
+	//               unk          unk
+	GPIO_Init(GPIOE, GPIO_PIN_3 | GPIO_PIN_5,
+				GPIO_MODE_IN_PU_NO_IT);
+	//               unk          unk
+	GPIO_Init(GPIOG, GPIO_PIN_0 | GPIO_PIN_1,
+				GPIO_MODE_IN_PU_NO_IT);
 }
 
 void debug_pin_init (void)
