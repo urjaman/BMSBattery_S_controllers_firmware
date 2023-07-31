14973d8a9fca896d92fe12691191bf88724ee55e
Urja Rannikko <urjaman@gmail.com>
2023-07-31 16:43:42 +0000

    personal junk

--- config.h
+++ config.h
@@ -9,18 +9,18 @@
 #define CONFIG_H_
 
 #define NUMBER_OF_PAS_MAGS 8
-#define limit 36
+#define limit 43
 #define timeout 7812
 #define wheel_circumference 1055L
 #define limit_without_pas 24
 #define ADC_THROTTLE_MIN_VALUE 72
 #define ADC_THROTTLE_MAX_VALUE 172
 #define BATTERY_VOLTAGE_MIN_VALUE 111
-#define BATTERY_CURRENT_MAX_VALUE 67L
-#define PHASE_CURRENT_MAX_VALUE 133L
-#define REGEN_CURRENT_MAX_VALUE 10L
-#define MOTOR_ROTOR_DELTA_PHASE_ANGLE_RIGHT 205
-#define current_cal_a 67
+#define BATTERY_CURRENT_MAX_VALUE 85L
+#define PHASE_CURRENT_MAX_VALUE 150L
+#define REGEN_CURRENT_MAX_VALUE 15L
+#define MOTOR_ROTOR_DELTA_PHASE_ANGLE_RIGHT 200
+#define current_cal_a 71
 #define LEVEL_1 40
 #define LEVEL_2 40
 #define LEVEL_3 40
@@ -37,16 +37,16 @@
 #define RAMP_START 64000
 #define limit_with_throttle_override 80
 #define CORRECTION_AT_ANGLE 127
+#define DIAGNOSTICS
 #define ANGLE_4_0 1
 #define ANGLE_6_60 43
 #define ANGLE_2_120 86
 #define ANGLE_3_180 128
-#define BLUOSEC
 #define ANGLE_1_240 171
 #define ANGLE_5_300 213
 #define TQS_CALIB 0.0
 #define ACA 4296
-#define EEPROM_INIT_MAGIC_BYTE 30 // makes sure (chance of fail 1/255) eeprom is invalidated after flashing new config
+#define EEPROM_INIT_MAGIC_BYTE 109 // makes sure (chance of fail 1/255) eeprom is invalidated after flashing new config
 #define ADC_BATTERY_VOLTAGE_K 69
 #define ACA_EXPERIMENTAL 1152
 #define BATTERY_VOLTAGE_MAX_VALUE 158
--- main.c
+++ main.c
@@ -92,8 +92,12 @@ void UART2_IRQHandler(void) __interrupt(UART2_IRQHANDLER);
 volatile int32_t test_value = -15000;
 
 extern int8_t hall_sensors;
+#define F_SETPT 0
 
 int main(void) {
+#if F_SETPT
+	int16_t f_setpt = 0;
+#endif
 	//set clock at the max 16MHz
 	CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);
 
@@ -145,12 +149,14 @@ int main(void) {
 #endif
 
 	int32_t test_result = test_value * 100;
-	
+
+#if 0
 	printf("test_result = %04X trh=%04X\n", (uint16_t)test_result, (uint16_t)(((uint32_t)test_result) >> 16));
 
 		printf("%03d %03d %03d %03d %03d %03d\r\n",
 			ui8_s_hall_angle4_0, ui8_s_hall_angle6_60, ui8_s_hall_angle2_120,
 			ui8_s_hall_angle3_180, ui8_s_hall_angle1_240, ui8_s_hall_angle5_300);
+#endif
 
 	while (1) {
 
@@ -182,6 +188,9 @@ int main(void) {
 			cruise_control_update();
 
 			ui16_setpoint = (uint16_t) aca_setpoint(ui16_time_ticks_between_pas_interrupt, ui16_setpoint); //update setpoint
+#if F_SETPT
+			if (ui16_setpoint < f_setpt) ui16_setpoint = f_setpt;
+#endif
 			pwm_set_duty_cycle((uint8_t) ui16_setpoint);
 			motor_slow_update_post();
 
@@ -202,14 +211,24 @@ int main(void) {
 #ifdef DIAGNOSTICS
 
 #if 1
-				printf("C%04X BC%03d/%03d PAS%u-%05u S%u SP%03d PCV%d B%u\r\n",
+				printf("C%04X BC%03d/%03d "
+				//"PAS%u-%05u "
+				"S%u SP%03d "
+				"MA%03d "
+				"PCV%d "
+				"IQ%04d"
+				//"B%u"
+				"\r\n",
 				ui16_control_state,
 				(int)(ui16_BatteryCurrent - ui16_current_cal_b),
 				(int)(uint32_current_target - ui16_current_cal_b),
-				PAS_is_active, ui16_time_ticks_between_pas_interrupt,
+//				PAS_is_active, ui16_time_ticks_between_pas_interrupt,
 				ui16_motor_speed_erps,
-				ui16_setpoint, ui8_position_correction_value,
-				ui8_BatteryVoltage
+				ui16_setpoint,
+				ui8_s_motor_angle,
+				ui8_position_correction_value,
+				ui16_ADC_iq_current
+				//ui8_BatteryVoltage
 				);
 #endif
 
@@ -239,10 +258,30 @@ int main(void) {
 #endif
 
 				if (ch >= 0) {
-					printf("CH:%02X\r\n", ch);
-#if 0
+				//	printf("CH:%02X\r\n", ch);
 					if (ch == 'x') ui8_s_motor_angle++;
 					if (ch == 'z') ui8_s_motor_angle--;
+#if 0
+					if (ch == 'a') f_setpt--;
+					if (ch == 's') f_setpt += 5;
+					if (ch == 'q') f_setpt = 0;
+					if ((ch == 'w')&&(f_setpt)) f_setpt = 64;
+					if (ch == 'c') ui16_aca_experimental_flags &= ~(USE_ALTERNATE_WAVETABLE|USE_ALTERNATE_WAVETABLE_B);
+					if (ch == 'v') {
+						ui16_aca_experimental_flags &= ~(USE_ALTERNATE_WAVETABLE|USE_ALTERNATE_WAVETABLE_B);
+						ui16_aca_experimental_flags |= USE_ALTERNATE_WAVETABLE; // pure sine
+					}
+					if (ch == 'b') {
+						ui16_aca_experimental_flags &= ~(USE_ALTERNATE_WAVETABLE|USE_ALTERNATE_WAVETABLE_B);
+						ui16_aca_experimental_flags |= USE_ALTERNATE_WAVETABLE_B; // third harmonic
+					}
+					if (ch == 'n') {
+						ui16_aca_experimental_flags |= USE_ALTERNATE_WAVETABLE | USE_ALTERNATE_WAVETABLE_B; // nip tuck
+					}
+
+#endif
+
+#if 0
 					if (ch == '8') ui8_s_motor_angle = (MOTOR_ROTOR_DELTA_PHASE_ANGLE_RIGHT-85) & 0xFF;
 					if (ch == '9') ui8_s_motor_angle = MOTOR_ROTOR_DELTA_PHASE_ANGLE_RIGHT;
 					if (ch == '0') ui8_s_motor_angle = (MOTOR_ROTOR_DELTA_PHASE_ANGLE_RIGHT+85) & 0xFF;
@@ -250,7 +289,7 @@ int main(void) {
 #endif
 				}
 
-				
+
 				if ((hall_sensors&1) != last_hallL) {
 					hall_changes++;
 					last_hallL = hall_sensors&1;
@@ -269,7 +308,7 @@ int main(void) {
 				putchar(255);*/
 				// printf("%u, %d, %d, %d, %d, %d, %d\r\n", ui8_position_correction_value, (uint16_t) uint8_t_hall_case[0], (uint16_t)uint8_t_hall_case[1],(uint16_t) uint8_t_hall_case[2],(uint16_t) uint8_t_hall_case[3], (uint16_t)uint8_t_hall_case[4], (uint16_t)uint8_t_hall_case[5]);
 	//			 printf("\r%03d %03d %03d %03d %03d %03d ", (uint16_t) uint8_t_hall_case[0], (uint16_t)uint8_t_hall_case[1],(uint16_t) uint8_t_hall_case[2],(uint16_t) uint8_t_hall_case[3], (uint16_t)uint8_t_hall_case[4], (uint16_t)uint8_t_hall_case[5]);
-				 
+
 		//		printf(" P%d, BC%d, SP%d, S%d, IQ%d, BV%d, MA%d, SAC%d, H%d",
 		//		ui8_position_correction_value, ui16_BatteryCurrent, ui16_setpoint, ui16_motor_speed_erps, ui16_ADC_iq_current>>2,ui16_adc_read_battery_voltage(), ui8_s_motor_angle,
 		//		pwm_swap_phases_ac, hall_sensors);
